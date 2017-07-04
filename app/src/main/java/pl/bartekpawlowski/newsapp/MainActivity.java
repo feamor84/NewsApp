8f430f7944079cb7ee7b6830f0c73d7ab262f02a
bartek <mail@bartekpawlowski.pl>
2017-07-04 22:40:06 +0200

    Add visual items when app is retriving data, add networks error handlers

--- app/src/main/java/pl/bartekpawlowski/newsapp/MainActivity.java
+++ app/src/main/java/pl/bartekpawlowski/newsapp/MainActivity.java
@@ -42,8 +42,6 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
     String noInternetConnection;
 
     // internet connection
-    private ConnectivityManager mConnectivityManager;
-    private NetworkInfo mNetworkInfo;
     private boolean isNetwork;
 
     @Override
